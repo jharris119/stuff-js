38d99229ac97f53cdd0834bcec1cc03df2bbf07d
Jay Harris <jayharris@gmail.com>
2014-01-27 11:42:41 -0500

    Random JavaScript utilities
    Stuff that might be useful in one or more projects in the future.

---
+++ underscore2.js
@@ -0,0 +1,15 @@
+/**
+ * Some additional functions I think would be nice to add to underscore.js.
+ */
+
+_.mixin({
+	
+	/**
+	 * @param {Any} num The thing we're testing for integer-ship.
+	 * @return {boolean} true if num is an integer (between Number.MIN_VALUE and
+	 * 	Number.MAX_VALUE) and false otherwise
+	 */
+	isInteger: function(num) {
+		return _.isFinite(num) && parseInt(num, undefined) == num;
+	}
+});
