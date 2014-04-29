fd67f805f8510679fc2f32026baf9c2507ed9436
Jay <jharris119@users.noreply.github.com>
2014-04-29 10:28:04 -0400

    Formatting...

--- underscore2.js
+++ underscore2.js
@@ -3,20 +3,20 @@
  */
 
 _.mixin({
-	/**
-	 * Keep calling func until it returns a non-undefined, non-null value.
-	 *
-	 * @param {function} func - the function to call
-	 * @param {object} context - the "this" object
-	 * @param {...*} args - arguments to func
-	 */
-	tryAgain: function(func, context, args) {
-		var res;
-		while (res === undefined || res === null) {
-			res = func.apply(context, Array.prototype.slice.call(arguments, 2));
-		}
-		return res;
-	},
+  /**
+   * Keep calling func until it returns a non-undefined, non-null value.
+   *
+   * @param {function} func - the function to call
+   * @param {object} context - the "this" object
+   * @param {...*} args - arguments to func
+   */
+  tryAgain: function(func, context, args) {
+    var res;
+    while (res === undefined || res === null) {
+      res = func.apply(context, Array.prototype.slice.call(arguments, 2));
+    }
+    return res;
+  },
 
   /**
    * A better typeof operator-function thing.
